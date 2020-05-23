1400c72aa35f9484b9ef4dd957533be1c31bc567
Jordan <jordanleelong5@gmail.com>
2020-05-22 19:02:31 -0700

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // Your code here
-let dodger = document.getElementById("dodger");
+let dodger = document.getElementById("dodger")
 dodger.style.backgroundColor = "#FF69B4";
-// dodger.style.height = "180px";
-// dodger.style.width = "600px";
+dodger.style.height = "180px";
+dodger.style.width = "50x";
 console.log(dodger.style.left); 
 console.log(dodger.style.right); 
 
 dodger.style.bottom = "0px";
 
 document.addEventListener("keydown", function(e) {
+    // adding eventlistener, when the key is pressed down it fires off a callback function
+    // if that key is equal to arrow left, call the move dodger left functoin
     if (e.key === 'ArrowLeft') {
        moveDodgerLeft();
     }
@@ -18,10 +20,17 @@ document.addEventListener("keydown", function(e) {
 
 function moveDodgerLeft() {
     let leftNumbers = dodger.style.left.replace("px", "");
-    let left = parseInt(leftNumbers, 10);
+    // defining variable left numbers, calling the dodger element adding the style and using .replace
+    //.replace searches a string for a specified value and returns a new string where the specified values are replaced
+    // the first arg "px" is the search value, the second value "" is the newvalue.. we use "" as a placeholder for the time being
 
+    let left = parseInt(leftNumbers, 10);
+// creating a variable left and parsing the string to an integer. 
+// calling left numbers, parsing that and passing in 10 as into the new value
     if (left > 0 ) {
+        // comparing left variable to 0 which results as true 
         dodger.style.left = `${left - 1}px`;
+        // every time the left arrow key is pressed it moves left one pixel
     }
 }
 
@@ -34,10 +43,16 @@ document.addEventListener("keydown", function(e) {
 
 function moveDodgerRight() {
     let leftNumbers = dodger.style.left.replace("px", "");
+    // doing the samw thing as above
     let left = parseInt(leftNumbers, 10);
+    // also doing the same thing as the move dodger left fucnction
 
     if (left > 0 ) {
         dodger.style.left = `${left + 1}px`;
+        // this time we are moving left + 1 so when the right arrow is pressed it moves to the right one pixel.
     }
 }
+
+
+
 
