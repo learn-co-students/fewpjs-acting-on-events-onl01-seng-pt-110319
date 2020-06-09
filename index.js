949e7550a74f58db2f12bbcf0106c4318fe36678
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-09 08:06:55 +0000

    Done.

--- index.js
+++ index.js
@@ -12,3 +12,19 @@
         moveDodgerLeft();
       }
     });
+    
+    function moveDodgerRight() {
+      let rightNumbers = dodger.style.left.replace("px", "");
+      let left = parseInt(rightNumbers, 10);
+      
+      if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+      }
+    }
+       document.addEventListener("keydown", function(e) {
+      if (e.key === "ArrowRight") {
+        moveDodgerRight();
+      }
+    });
+    
+    
