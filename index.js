8e57283d2d32355abd328b524b0ea94debb35056
jhar3ll <jharell.chambers@selu.edu>
2020-06-09 07:46:55 -0500

    Done.

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 function moveDodgerLeft() {
     let leftNumbers = dodger.style.left.replace('px', '');
-    let left = parseInt(leftNumbers, 10)
-    if (left > 0) {
-      dodger.style.left = `${left - 1}px`
+    let moveLeft = parseInt(leftNumbers, 10)
+    if (moveLeft > 0) {
+      dodger.style.left = `${moveLeft - 1}px`
     }
   }
   document.addEventListener('keydown', function(e) {
@@ -16,9 +16,9 @@ function moveDodgerLeft() {
   
   function moveDodgerRight() {
     let leftNumbers = dodger.style.left.replace('px', '');
-    let left = parseInt(leftNumbers, 10)
-    if (left > 0) {
-      dodger.style.left = `${left + 1}px`
+    let moveRight = parseInt(leftNumbers, 10)
+    if (moveRight > 0) {
+      dodger.style.left = `${moveRight + 1}px`
     }
   }
   document.addEventListener('keydown', function(e) {
